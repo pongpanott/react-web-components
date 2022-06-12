d1a3ab3852d9d93536b963ff1066f8fbd240545e
pongpanott <fhyptr@gmail.com>
2022-06-12 23:52:16 +0700

    feat: implement label component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import AppRouter from "./router/router";
+import AppRouter from './router/router';
 
 function App() {
-	return <AppRouter />;
+  return <AppRouter />;
 }
 
 export default App;
--- src/pages/text-field/text-field.page.tsx
+++ src/pages/text-field/text-field.page.tsx
@@ -1,25 +1,29 @@
-import { Form } from "react-final-form";
-import TextField from "../../components/text-field/text-field.component";
-import { useViewModel } from "./text-field-page.viewmodel";
+import { Form } from 'react-final-form';
+import TextField from '../../components/text-field/text-field.component';
+import { useViewModel } from './text-field-page.viewmodel';
 
 const TextFieldPage = () => {
-	const { validate, onSubmit } = useViewModel();
+  const { validate, onSubmit } = useViewModel();
 
-	return (
-		<div>
-			<Form
-				onSubmit={onSubmit}
-				validate={validate}
-				render={({ handleSubmit }) => (
-					<form onSubmit={handleSubmit}>
-						<TextField name="simpleText" />
+  return (
+    <div>
+      <Form
+        onSubmit={onSubmit}
+        validate={validate}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <TextField
+              name="simpleText"
+              label="simple text field"
+              placeholder="simple text field"
+            />
 
-						<button type="submit">submit</button>
-					</form>
-				)}
-			/>
-		</div>
-	);
+            <button type="submit">submit</button>
+          </form>
+        )}
+      />
+    </div>
+  );
 };
 
 export default TextFieldPage;
